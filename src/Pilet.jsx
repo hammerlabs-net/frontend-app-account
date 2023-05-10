7a486d9e5d6c4a9c1af48b41b21637df6113dc0b
pedromartello <martello@gmail.com>
2023-05-10 16:21:47 -0400

    Cleanup - moving message and config initialization back into the MFEs.

--- src/Pilet.jsx
+++ src/Pilet.jsx
@@ -8,6 +8,7 @@ import * as React from 'react';
 import AccountSettingsPage from './account-settings/AccountSettingsPage';
 import IdVerificationPage from './id-verification/IdVerificationPage';
 import reduxConfig from './account-settings/data/module';
+import appMessages from './i18n';
 
 const piletSpec = {
   name: 'openEdx Account MFE - Pilet Version',
@@ -17,6 +18,19 @@ const piletSpec = {
   config: {},
   basePath: '/pilets',
   setup(piralApi) {
+    piralApi.mergeMessages(appMessages);
+
+    piralApi.mergeConfig({
+      SUPPORT_URL: process.env.SUPPORT_URL,
+      COACHING_ENABLED: (process.env.COACHING_ENABLED || false),
+      ENABLE_DEMOGRAPHICS_COLLECTION: (process.env.ENABLE_DEMOGRAPHICS_COLLECTION || false),
+      DEMOGRAPHICS_BASE_URL: process.env.DEMOGRAPHICS_BASE_URL,
+      ENABLE_COPPA_COMPLIANCE: (process.env.ENABLE_COPPA_COMPLIANCE || false),
+      ENABLE_DOB_UPDATE: (process.env.ENABLE_DOB_UPDATE || false),
+      MARKETING_EMAILS_OPT_IN: (process.env.MARKETING_EMAILS_OPT_IN || false),
+      PASSWORD_RESET_SUPPORT_LINK: process.env.PASSWORD_RESET_SUPPORT_LINK,
+    }, 'App loadConfig override handler');
+
     piralApi.registerPage('/account', () => (
       <DynamicModuleLoader modules={[reduxConfig]}>
         <AccountSettingsPage />
--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,9 @@ import 'regenerator-runtime/runtime';
 import 'formdata-polyfill';
 import './index.scss';
 
-import appMessages from './i18n';
 import AccountPilet from './Pilet';
 
 import './account-settings/_style.scss';
 import './id-verification/_id-verification.scss';
-
-export const messages = appMessages;
 
 export default AccountPilet;
